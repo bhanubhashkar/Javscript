b179302f2f09dd823898c272fec7a83139cc3356
Bhanu Bhashkar <reachoutbhanu@gmail.com>
2024-05-12 12:00:49 +0530

    changes

---
+++ 06_Operators-Expressions.js
@@ -0,0 +1,64 @@
+
+// #####----- JavaScript Operators & Expressions -------------------------------------------------------------------------------------------#####
+
+
+/*
+Arithmetic Operators
+    +   :   Addition
+    -   :   Subtraction
+    *   :   Multiplication
+    **  :   Exponentiation (ES2016)
+    /   :   Division
+    %   :   Modulus (Division Remainder)
+    ++  :   Increment
+    --  :   Decrement
+
+Comparision Operator
+    ==  :   equal to
+    ===	:   equal value and equal type
+    !=	:   not equal
+    !==	:   not equal value or not equal type
+    >	:   greater than
+    <	:   less than
+    >=	:   greater than or equal to
+    <=	:   less than or equal to
+    ?	:   ternary operator
+
+Assignment Operator 
+    =   :   x = y	:   x = y
+    +=	:   x += y	:   x = x + y
+    -=	:   x -= y	:   x = x - y
+    *=	:   x *= y	:   x = x * y
+    /=	:   x /= y	:   x = x / y
+    %=	:   x %= y	:   x = x % y
+    **=	:   x **= y	:   x = x ** y
+
+Logical Operator
+    &&	:   logical and
+    ||	:   logical or
+    !	:   logical not
+
+Logical Assignment Operator	
+    Symbol      Example	        Same As
+    &&=	    :   x &&= y	    :   x = x && (x = y)
+    ||=	    :   x ||= y	    :   x = x || (x = y)
+    ??=	    :   x ??= y	    :   x = x ?? (x = y)
+
+Bitwise Operator	    
+    Symbol  Meaning     Example     Same as	        Result	    Decimal
+    &	:   AND	        5 & 1	    0101 & 0001	    0001	    1
+    |	:   OR	        5 | 1	    0101 | 0001	    0101	    5
+    ~	:   NOT	        ~ 5	        ~0101	        1010        10
+    ^	:   XOR	        5 ^ 1	    0101 ^ 0001	    0100	    4
+    <<	:   left shift	5 << 1	    0101 << 1	    1010	    10
+    >>	:   right shift	5 >> 1	    0101 >> 1	    0010	    2
+    >>>	:   unsigned right shift	5 >>> 1	0101 >>> 1	0010    2
+
+Bitwise Assignment Operator	
+    Symbol  Example	    Same As
+    &=	    x &= y	    x = x & y
+    ^=	    x ^= y	    x = x ^ y
+    |=	    x |= y	    x = x | y
+
+*/
+
