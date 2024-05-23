943cc7afb5e3b07286d30b6d2cec1289aedb7ab0
Bhanu Bhashkar <reachoutbhanu@gmail.com>
2024-05-24 00:31:26 +0530

    changes

---
+++ 15_PracticeSet04.js
@@ -0,0 +1,21 @@
+
+// #####----- JavaScript Practice Set 04  -----------------------------------------------------------------------------------------------#####
+
+const myName = "BhAnu\"s";
+console.log(myName.length);
+
+console.log(myName.includes("Bh"));
+console.log(myName.includes("as"));
+
+console.log(myName.startsWith("B"));
+console.log(myName.startsWith("h"));
+
+
+console.log(myName.endsWith("s"));
+console.log(myName.endsWith("S"));
+
+console.log(myName.toLowerCase());
+
+const getPrice ="The Price is 1000 INR."
+
+console.log(parseInt(getPrice.slice(13,18)));
